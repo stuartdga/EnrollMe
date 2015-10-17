8d667b783b9390ea9579fc616620ecb81322761d
stuartdga <stuartdga@yahoo.com>
2015-10-17 12:25:06 -0400

    Updates for AtlantaCodeCamp 2015

---
+++ EnrollMe.Tests/HealthTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+using APICommon;
+using EnrollMe;
+
+namespace EnrollMe.Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class HealthTests
+    {
+        public HealthTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void HealthController_Get()
+        {
+            var controller = new EnrollMe.Controllers.HealthController();
+            Assert.IsTrue(controller.Get().StartsWith("I am working as expected for GET."));
+        }
+    }
+}
---
+++ EnrollMe/Controllers/HealthController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APICommon;
+
+namespace EnrollMe.Controllers
+{
+    public class HealthController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "I am working as expected for GET.  Build Time: " + APICommon.Helper.GetAssemblyBuildTimestamp().ToString();
+        }
+
+        [HttpGet]
+        public string Get(string action)
+        {
+            if (action.Equals("exception", StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new ApplicationException("Test Exception"); 
+            }
+
+            return "I am working as expected for GET.  Build Time: " + APICommon.Helper.GetAssemblyBuildTimestamp().ToString();
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] TestObject testObject)
+        {
+            var responseObject = new ResponseObject() {
+                id = testObject.id,
+                build = APICommon.Helper.GetAssemblyBuildTimestamp(),
+            };
+            var response = Request.CreateResponse(HttpStatusCode.OK, responseObject);    
+            response.Headers.Add("id", testObject.id.ToString());
+            return response;
+        }
+    }
+
+    public class TestObject
+    {
+        public int id { get; set; }
+        public DateTime? buildtime { get; set; }
+    }
+    public class ResponseObject
+    {
+        public int id { get; set; }
+        public DateTime? build { get; set; }
+    }
+
+}
